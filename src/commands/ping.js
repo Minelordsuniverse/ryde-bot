105eae0670fb80fecf901f0d964f6cadd87bf902
DefinitelyNotML <67429188+Minelordsuniverse@users.noreply.github.com>
2026-03-20 04:34:32 +0800

    Create ping.js
    for commands folder as well

---
+++ src/commands/ping.js
@@ -0,0 +1,26 @@
+// NOTE AS OF 20/3/2026: PLACEHOLDER FILE -- WILL BE REPLACED OR REMOVED
+const { SlashCommandBuilder } = require("discord.js");
+
+module.exports = {
+  data: new SlashCommandBuilder()
+    .setName("ping")
+    .setDescription("Replies with the bot latency."),
+
+  /**
+   * @param {import("discord.js").ChatInputCommandInteraction} interaction
+   * @param {import("discord.js").Client} client
+   */
+  async execute(interaction, client) {
+    const sent = await interaction.reply({
+      content: "Pinging…",
+      fetchReply: true,
+    });
+
+    const roundtrip = sent.createdTimestamp - interaction.createdTimestamp;
+    const wsHeartbeat = client.ws.ping;
+
+    await interaction.editReply(
+      `🏓 Pong!\n> Roundtrip: **${roundtrip}ms** | WebSocket: **${wsHeartbeat}ms**`
+    );
+  },
+};
